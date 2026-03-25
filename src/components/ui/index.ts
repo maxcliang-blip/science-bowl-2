0baa2fb66db21b59e660252a5cfedcea8ef174c2
[dyad] <max.c.liang@gmail.com>
2026-03-25 18:26:20 -0400

    [dyad] wrote 9 file(s)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,7 @@
 export { Button, buttonVariants } from "./button";
 export { Popover, PopoverTrigger, PopoverContent } from "./popover";
+export { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./alert-dialog";
+export { Calendar } from "./calendar";
+export { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./carousel";
+export { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "./pagination";
+export { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupAction, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarInput, SidebarInset, SidebarMenu, SidebarMenuAction, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubItem, SidebarProvider, SidebarTrigger, useSidebar } from "./sidebar";
